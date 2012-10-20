6cd3257cc6b6613f13a360bb1f2dfce2f7cf3620
cyclicalnight <hazimu223@gmail.com>
2012-10-20 17:25:40 +0900

    init

---
+++ app/view/Main.js
@@ -0,0 +1,20 @@
+Ext.define("RF.view.Main", {
+    extend: 'Ext.tab.Panel',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [
+                {
+                	xtype: 'homepanel'
+                },
+                {
+                	xtype: 'getstarted'
+                
+                }
+        ]
+    }
+});
